b049dd8ae8369f33885bf163a06a3226f0e19b3c
Sander Dudok <sander.dudok@gmail.com>
2018-03-28 23:01:55 +0200

    selected added to list

--- myApp.js
+++ myApp.js
@@ -1,10 +1,22 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $http) {
-    $http.get("https://api.coinmarketcap.com/v1/ticker/?limit=100")
-        .then(function mySuccess(response) {
-        $scope.data = response.data;
-    }, function myError(response) {
-        $scope.error = response.error;
-    });
+var app = angular.module('exchangeInfo', []);
+
+app.controller('checkboxCtrl', function($scope) {
+    
+    $scope.exchanges = ccxt.exchanges;
+    $scope.selection = ['coinmarketcap'];
+    $scope.toggleSelection = function toggleSelection(exchange) {
+        // Toggle selection for a given exchange
+        var i = $scope.selection.indexOf(exchange);
+        // Is currently selected
+        if (i > -1) {
+            $scope.selection.splice(i, 1);
+        }
+        // Is newly selected
+        else {
+            $scope.selection.push(exchange);
+        }
+    };
 });
+    
+
 
